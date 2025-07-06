2aa223cb21b92554b730092f589161fc2e040479
Mg Phone <khantalfred832@gmail.com>
2025-07-06 11:31:36 +0700

    Create main.py

---
+++ remove_duplicates/main.py
@@ -0,0 +1,13 @@
+items = input().split(',')
+
+seen = set()
+unique_items = []
+
+for item in items:
+    item = item.strip()
+    if item not in seen:
+        unique_items.append(item)
+        seen.add(item)
+
+for item in unique_items:
+    print(item)
